fb097afce942f5a839386dedae01af8df6326b50
YoucOuss <youcef-pino@hotmail.fr>
2018-02-16 12:25:13 -0500

    ajout de la classe client

---
+++ src/Client.java
@@ -0,0 +1,43 @@
+
+
+public class Client {
+
+	/*Attributs*/
+	private String nom;
+	private double facture;
+
+	/*Constructeur avec un paramètre*/
+	public Client(String nom) {
+
+		this.nom = nom;
+		this.facture = 0;
+	}
+
+	/*Accesseur de l'attribut nom*/
+	public String getNom() {
+		return nom;
+	}
+
+	/*Mutateur de l'attribut nom*/
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	/*Accesseur de l'attribut nom*/
+	public double getFacture() {
+		return facture;
+
+	}
+	
+	/*Mutateur de l'attribut facture*/
+	public void setFacture(double facture) {
+		this.facture = facture;
+
+	}
+	
+	/*Affiche la facture*/
+	public void afficher() {
+		System.out.println(this.getNom() + " " + String.format("%.2f$", this.getFacture()));
+	}
+
+}
--- src/GestionCommandes.java
+++ src/GestionCommandes.java
@@ -1,4 +1,4 @@
-/*partie Yousef*/
+/*partie Youcef*/
 import java.util.List;
 import java.io.BufferedReader;
 import java.io.IOException;
